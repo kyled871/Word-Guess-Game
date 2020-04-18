6ee71582aea1b71fde40c5598c11db9c73f9c717
Kyle Davis <kyled871@yahoo.com>
2020-04-18 16:34:06 -0400

    created next level function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ const gameWords = [
 ]
 
 // randomizes the words in the array
-const currentWord = gameWords[Math.floor(Math.random() * gameWords.length)].toUpperCase();
+let currentWord = gameWords[Math.floor(Math.random() * gameWords.length)].toUpperCase();
 
 let guess;
 let board = [];
@@ -15,8 +15,7 @@ let wrongLetters = [];
 // displays current lives and score
 let lives = 5;
 document.getElementById("lives").innerHTML = lives;
-
-points = 0;
+let points = 0;
 let score = 0;
 document.getElementById("score").innerHTML = score;
 
@@ -34,8 +33,6 @@ function start() {
     }
 
     document.getElementById("currentWord").innerHTML = board.join("");
-    
-
     // removed commas in array with .join:
     // https://stackoverflow.com/questions/12835621/removing-commas-from-javascript-array
     
@@ -48,42 +45,66 @@ function checkInput() {
 
     document.onkeyup = function(event) {
 
+        // the users guess is converted into uppercase
         guess = event.key.toUpperCase();
         let found = false;
 
+        // the users guess is added on to the board if correct
         for (i = 0; i < currentWord.length; i++) {
             if (guess === currentWord[i]) {
             board[i] = guess;
             document.getElementById("currentWord").innerHTML = board.join(" ");
-            found = true;
             points++
+            console.log(points)
+            found = true;
+            
             }
         }
 
-        if (points === board.length) {
+        // if the points variable = however long the word is. User gets 1 point and goes to next round.
+        if (points === currentWord.length) {
             score++;
-            console.log(score)  // cant get to work without having console.log(score)
             document.getElementById("score").innerHTML = score;
+            alert("Next round!")
+            lives = 5;
+            points = 0;
+            document.getElementById("lives").innerHTML = lives;
+            currentWord = gameWords[Math.floor(Math.random() * gameWords.length)].toUpperCase();
+            for (let i = 0; i < currentWord.length; i++) {
+                board[i] = " _ ";
+                }
+            document.getElementById("currentWord").innerHTML = board[i];
+            console.log(currentWord)
+            
         }
 
         if (found) return;
 
+        // if you guess a wrong letter, lives go down by 1 increment and that letter is shown
         if (wrongLetters.indexOf(guess) < 0) {
             wrongLetters.push(guess);
             document.getElementById("wrongLetters").innerHTML = wrongLetters.join(" ");
             lives--;
             document.getElementById("lives").innerHTML = lives;
-
         }
 
+        if (found = true) {
+        }
+        // gameover - resets page
         if (lives == 0) {
             alert("Better luck next time!");
             resetGame()
         }
+
+
+
+
+
     }
 }
+console.log(currentWord)
+console.log(currentWord.length)
 
 start()
 checkInput()
-
 
